70a0ec6952f9b0ef32996b08d5a8eef3866a1826
antestor86 <polygon86.86@gmail.com>
2021-06-03 01:35:03 +0400

    edit login menu

--- Angular/Customer/src/app/home/home.component.ts
+++ Angular/Customer/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  form:FormGroup|any
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup(
+      {
+        username:new FormControl("",Validators.required),
+        password:new FormControl(null,[Validators.minLength(6),Validators.required])
+      }
+    )
+  }
+
+  login(){
+    const formData = {...this.form.value}
+    console.log(formData)
   }
 
 }
