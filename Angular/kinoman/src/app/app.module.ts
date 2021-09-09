2c65fb8f612bbbc8477d702b324768d9d24073ac
antestor86 <polygon86.86@gmail.com>
2021-09-10 00:25:48 +0400

    make new project

---
+++ Angular/kinoman/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { Routes,RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { InfoComponent } from './info/info.component';
+import { TemplateComponent } from './template/template.component';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    InfoComponent,
+    TemplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/kinoman/src/app/main/main.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+export interface Movie{
+  id:number,
+  title:string,
+  year:number,
+  genre:string,
+  country:string,
+  rate:string,
+  poster:string
+}
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  movies:Movie[] = [];
+  constructor(private http:DataService) { }
+  imageSource:string [] = [];
+
+  ngOnInit(): void {
+
+    this.getData();
+    console.log(this.movies)
+
+
+  }
+
+  getData(){
+    this.http.getData().subscribe((data:any)=>{
+      for(let movie of data){
+          this.movies.push(movie)
+      }
+    })
+
+  }
+
+}
