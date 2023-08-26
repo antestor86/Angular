e31c0fe329e92822c65b246aef670db090d5b38b
antestor86 <polygon86.86@gmail.com>
2023-08-26 19:51:06 +0400

    room1

---
+++ Python/Skillbox/Beginning/lesson_005/01_inhabitants.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import room_1
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+for item in room_1.folks:
+    print(f"В комнате room1 живут ",item)
+
+
