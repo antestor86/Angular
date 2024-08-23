49ceab721e308c52e1540082bbb600d59567154b
Razmik Avetisyan <54534919+antestor86@users.noreply.github.com>
2024-08-24 00:38:13 +0400

    Replace Values option 1

---
+++ Python/replace values.py
@@ -0,0 +1,14 @@
+#replace fluids in  the cups
+orange_juise_cup = "Orange Juice "
+water_cup = "water"
+temporary_cup = ""
+
+print("In the orange juice cup ",orange_juise_cup)
+print("In the water cup is the  ",water_cup)
+
+temporary_cup = orange_juise_cup
+orange_juise_cup = water_cup
+water_cup = temporary_cup
+
+print("In the orange juice cup ",orange_juise_cup)
+print("In the orange juice cup  ",water_cup)
